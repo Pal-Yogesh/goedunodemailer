e16682e59766e073306e5909cc155aa808392026
Yogesh Pal <yogeshpal5049@gmail.com>
2024-12-10 11:21:58 +0530

    nodemailer code updated for mutiple websites

--- service.js
+++ service.js
@@ -2,17 +2,20 @@ const express = require("express");
 const nodemailer = require("nodemailer");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+
 const app = express();
 const PORT = 8080;
+
 app.use(
   cors({
-    origin: "*", // Replace '*' with your domain for security
+    origin: "*", // Replace '*' with specific domains for production
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
@@ -22,7 +25,7 @@ const transporter = nodemailer.createTransport({
 });
 
 app.post("/send-email", async (req, res) => {
-  const { name, email, phone, classes } = req.body;
+  const { name, email, phone, classes, source } = req.body; 
 
   // Build email text dynamically based on provided fields
   const details = [
@@ -30,11 +33,12 @@ app.post("/send-email", async (req, res) => {
     phone ? `Mobile : ${phone}` : null,
     classes ? `Class  : ${classes}` : null,
     email ? `Email  : ${email}` : null,
+    source ? `Source : ${source}` : null, 
   ]
-    .filter(Boolean) // Remove `null` values
+    .filter(Boolean)
     .join("\n");
 
-  const subject = `Enquiry from ${name || "Unknown User"}`;
+  const subject = `Enquiry from ${name || "Unknown User"} - [${source || "Unknown Source"}]`;
   const text = `
     -------------------------
            User Details
